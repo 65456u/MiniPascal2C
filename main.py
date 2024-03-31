da5e1a270bd812b6c78ee600cd1bfba3e1776472
65456u <yanghongchen@bupt.edu.cn>
2024-03-31 11:21:04 +0800

    parser

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import lark
-
-
-def main():
-    parser = lark.Lark(r"""
-        start: "hello" NAME "!"
-
-        %import common.CNAME -> NAME
-        %import common.WS
-        %ignore WS
-    """)
-    print(parser.parse("hello World!").pretty())
-    return 0
-
-
-main()
+import mp2c
