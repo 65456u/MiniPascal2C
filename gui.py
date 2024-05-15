6f32504abcdf49067b262d4ac5a3258192981ddd
Koishi <1421807615@qq.com>
2024-05-15 21:40:54 +0800

    gui_debug6

--- gui.py
+++ gui.py
@@ -84,7 +84,8 @@ def compile_txt(source, target):
         t_line_numbers.tag_add("coloredText", end_index, tk.END)
         t_line_numbers.tag_config("coloredText", foreground = "red")
         # 错误信息写入target文本框
-        outputErrorMsg = errorMsg.split("error: ", 1)[1]
+        #outputErrorMsg = errorMsg.split("error: ", 1)[1]
+        outputErrorMsg = compileRes.error_messages[0]
         end_index = target.index(tk.END)
         target.insert(tk.END, "\n\n" + outputErrorMsg)
         target.tag_add("coloredText", end_index, tk.END)
